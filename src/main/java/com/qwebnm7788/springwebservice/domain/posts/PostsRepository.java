5e4345079d89b934e57cfa985052b83ae508b74b
qwebnm7788 <qwebnm7788@naver.com>
2018-12-27 14:29:22 +0900

    JPA test 추가

---
+++ src/main/java/com/qwebnm7788/springwebservice/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.qwebnm7788.springwebservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
--- src/main/java/com/qwebnm7788/springwebservice/web/WebRestController.java
+++ src/main/java/com/qwebnm7788/springwebservice/web/WebRestController.java
@@ -1,14 +1,26 @@
 package com.qwebnm7788.springwebservice.web;
 
+import com.qwebnm7788.springwebservice.domain.posts.PostsRepository;
+import com.qwebnm7788.springwebservice.domain.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
 
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
+    }
 }
