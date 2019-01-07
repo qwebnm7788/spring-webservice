d8084a1a52d3df771db4f641226611170881ff88
qwebnm7788 <qwebnm7788@naver.com>
2019-01-07 17:01:05 +0900

    Add profile configuration
    - Add test code for profiles

--- src/main/java/com/qwebnm7788/springwebservice/web/WebRestController.java
+++ src/main/java/com/qwebnm7788/springwebservice/web/WebRestController.java
@@ -4,16 +4,20 @@ import com.qwebnm7788.springwebservice.domain.posts.PostsRepository;
 import com.qwebnm7788.springwebservice.domain.posts.PostsSaveRequestDto;
 import com.qwebnm7788.springwebservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -23,5 +27,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto) {
         postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
